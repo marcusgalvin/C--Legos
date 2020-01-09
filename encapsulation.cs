daaca82752c99b8aed2bf9f353fbe6b847f8835c
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2020-01-08 20:15:48 -0500

    encapsulation

--- encapsulation.cs
+++ encapsulation.cs
@@ -1,32 +1,33 @@
+//encapsulation
 using System;
 
 namespace Bulldozer
 {
-    class MainClass
+  class MainClass
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var worker = new FactoryWorker();
-            var result = worker.AddNumbers(2, 3);
+      var worker = new FactoryWorker();
+      var result = worker.AddNumbers(2, 3);
 
-            Console.WriteLine(result.ToString());
-            
-        }
+      Console.WriteLine(result.ToString());
+
     }
+  }
 
-    public class FactoryWorker
+  public class FactoryWorker
+  {
+    public FactoryWorker()
     {
-        public FactoryWorker()
-        {
 
-        }
-        public int AddNumbers(int num1, int num2)
-        {
-            var result = num1 + num2;
-
-            result = result + 100;
-            result = result - 100;
-            return result;
-        }
     }
+    public int AddNumbers(int num1, int num2)
+    {
+      var result = num1 + num2;
+
+      result = result + 100;
+      result = result - 100;
+      return result;
+    }
+  }
 }
